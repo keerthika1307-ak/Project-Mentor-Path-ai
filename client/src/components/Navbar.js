4502cc974fb94243d48b34003e9c77e3682893f5
keerthika1307-ak <keerthikaanandhan06@gmail.com>
2025-08-28 12:20:14 +0530

    Modified

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = ({ userRole, userName }) => {
             
             <button 
               className="nav-link" 
-              onClick={() => navigate(/${userRole}`)}
+              onClick={() => navigate(`/${userRole}`)}
             >
               Dashboard
             </button>
